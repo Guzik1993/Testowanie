7d754ad36bb5141b9a804b715e95d4d5d53d198b
Guzik1993 <andrzej.rzeznik.fm@gmail.com>
2017-03-07 12:58:55 +0100

    Dodanie testowego programu

---
+++ silnia.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+    int main ()
+    {
+        int n=11;
+        int tab[n];
+        int silnia;
+        printf ("Podaj 11 liczb naturalnych:\n");
+        scanf ("%d",&n);
+    for (n=0;n<=tab[11];n++)
+    {
+        silnia=silnia*n;
+    }
+    printf ("Silnia liczby %d wynosi %d:\n",n,silnia);
+    return 0;
+}
